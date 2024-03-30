dd3d54a009c4c67b62e2eedeee63f6fa407187ff
olya_nick <olya17nikolbas@gmail.com>
2024-03-30 10:34:14 +0300

    Добавлен Телефон в модель

--- PatientService.Domain/Entities/Patient.cs
+++ PatientService.Domain/Entities/Patient.cs
@@ -12,6 +12,6 @@ namespace PatientService.Domain.Entities
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime DateOfBirth { get; set; }
-
+        public int PhoneNumber { get; set; }
     }
 }
