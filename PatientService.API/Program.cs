09a7d2fed69e47066d61b7c14c6b45cdc2d4f4d5
olya_nick <olya17nikolbas@gmail.com>
2024-04-11 02:09:03 +0300

    Убран файл Startup, перенесен в стандартный метод Program

--- PatientService.API/Program.cs
+++ PatientService.API/Program.cs
@@ -1,17 +1,88 @@
+using MassTransit;
+using Microsoft.OpenApi.Models;
+using PatientService.API.Consumers;
+using PatientService.API.Settings;
+using PatientService.Data.Context;
+using PatientService.Data.Repositories;
+using PatientService.Domain.Repositories;
+using PatientService.Domain.Services;
+
 namespace PatientService.API
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var builder = WebApplication.CreateBuilder(args);
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile("appsettings.Development.json", true)
+                .Build();
+
+            if (configuration.Get<ApplicationSettings>() is not ApplicationSettings receptionConfig)
+                throw new ConfigurationException("Ошибка при подключении к ApplicationSettings");
+
+            string connection = configuration!.GetConnectionString("DefaultConnection");
+            if (string.IsNullOrEmpty(connection))
+                throw new ConfigurationException("Связь не установлена");
+
+            builder.Services.AddTransient<IPatientRepository, PatientRepository>();
+
+            // Add services to the container.
+            builder.Services.AddControllers();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+                c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+            });
+            builder.Services.AddMassTransit(x =>
+            {
+                x.AddConsumer<PatientConsumer>();
+
+                x.UsingRabbitMq((context, cfg) =>
+                {
+                    cfg.Host(receptionConfig.RabbitMqSetting.Host, receptionConfig.RabbitMqSetting.VirtualHost, h =>
+                    {
+                        h.Username(receptionConfig.RabbitMqSetting.UserName);
+                        h.Password(receptionConfig.RabbitMqSetting.Password);
+                    });
+
+                    cfg.UseTransaction(_ =>
+                    {
+                        _.Timeout = TimeSpan.FromSeconds(60);
+                    });
+
+                    cfg.ReceiveEndpoint(new TemporaryEndpointDefinition(), e =>
+                    {
+                        e.ConfigureConsumer<PatientConsumer>(context);
+                    });
+                    cfg.ConfigureEndpoints(context);
+                });
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger(c =>
+                {
+                    c.RouteTemplate = "/swagger/{documentName}/swagger.json";
+                });
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+            }
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
     }
 }
