e7d6735097efe306a471d2ad3ed86afb3af8383c
olya_nick <olya17nikolbas@gmail.com>
2024-04-11 02:10:29 +0300

    Реализован класс PatientService

---
+++ PatientService.Domain/Services/PatientService.cs
@@ -0,0 +1,25 @@
+﻿using HelpersDTO.CallCenter.DTO.Models;
+using PatientService.Domain.Entities;
+using PatientService.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatientService.Domain.Services
+{
+    public class PatientService : IPatientService
+    {
+
+        public Task<Patient?> GetPatientById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Guid> IPatientService.AddPatient(PatientDto patient)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
